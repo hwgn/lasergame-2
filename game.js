d91aaceb5b7d6fd82627b2d474c3389794b9152f
Hendrik Wagner <hendrik.wagner@mni.thm.de>
2023-03-30 18:22:14 +0200

    refactor to ease playing in p5.js editor

--- game.js
+++ game.js
@@ -1,10 +1,14 @@
-export function setup(p5 = this, canvasParentRef = undefined) {
+// remove the following line to execute in the p5.js editor (https://editor.p5js.org/)
+// or alternatively play the game at https://hendrikwagner.de/projects/lasergame-2/
+export { setup, draw };
+
+function setup(p5 = this, canvasParentRef = undefined) {
   p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
   p5.pixelDensity(2);
 
 }
 
-export function draw(p5 = this) {
+function draw(p5 = this) {
   p5.background(0);
   p5.fill(255);
   p5.rect(100, 100, 200, 200);
